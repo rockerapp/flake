96d00f753837dc3a096ac32fcf5b6ce6fd32c3f9
Haden Fletcher <haden@hadenfletcher.com>
2020-06-11 21:09:35 -0500

    clean up internal syntax
    Signed-off-by: Haden Fletcher <haden@hadenfletcher.com>

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,62 @@
-export interface FlakeIDParams {
-  seq?: number,
-  mid?: number,
-  timeOffset?: number
-  lastTime?: number
+export interface FlakeOptions {
+	/**
+	 * Unique machine ID.
+	 */
+	nodeId?: number
+	/**
+	 * Time, in milliseconds, to subtract from current time during ID generation. Based on Unix epoch.
+	 */
+	timeOffset?: number
 }
-export default class FlakeId {
-  public seq: number
-  public mid: number
-  public timeOffset: number
-  public lastTime: number
 
-  constructor({ seq = 0, mid = 1, timeOffset = 0, lastTime = 0 }: FlakeIDParams = {}) {
-    this.seq = seq
-    this.mid = mid % 1023;
-    this.timeOffset = timeOffset;
-    this.lastTime = lastTime;
-  }
+export default class Flake {
+	private sequence: number = 0
+	private lastTime: number = 0
 
-  gen() {
-    const time = Date.now(),
-    bTime = (time - this.timeOffset).toString(2);
+	private nodeId: number = 1
+	private timeOffset: number = 0
 
-    // get the sequence number
-    if (this.lastTime == time) {
-      this.seq += 1;
+	constructor(options: FlakeOptions) {
+		this.nodeId = (options.nodeId || this.nodeId) % 1023
+		this.timeOffset = options.timeOffset || this.timeOffset
+	}
 
-      if (this.seq > 4095) {
-        this.seq = 0;
+	generateRaw(): BigInt {
+		const nowTime = Date.now()
+		const genTime = (nowTime - this.timeOffset).toString(2)
 
-        // make system wait till time is been shifted by one millisecond
-        while (Date.now() <= time) {}
-      }
-    } else {
-      this.seq = 0;
-    }
+		// set sequence number
+		// this prevents multiple IDs from being generated in 1ms
+		if (this.lastTime === nowTime) {
+			this.sequence += 1
 
-    this.lastTime = time;
+			if (this.sequence > 4095) {
+				this.sequence = 0
 
-    // make sure sequence length will be constant
-    const bSeq = this.seq.toString(2).padStart(12, "0");
-    const bMid = this.mid.toString(2).padStart(10, "0");
-    const bid = bTime + bMid + bSeq;
+				// wait until time has incremented by a millisecond
+				while (Date.now() <= nowTime) { }
+			}
+		} else {
+			this.sequence = 0
+		}
 
-    let id = "";
+		this.lastTime = nowTime
 
-    for (let i = bid.length; i > 0; i -= 4) {
-      id = parseInt(bid.substring(i - 4, i), 2).toString(16) + id;
-    }
+		// make sure sequence length will be constant
+		const genSequence = this.sequence.toString(2).padStart(12, "0")
+		const genNode = this.nodeId.toString(2).padStart(10, "0")
+		const rawId = genTime + genNode + genSequence
 
-    return BigInt(`0x${id}`);
-  }
+		let id = ""
+
+		for (let i = rawId.length; i > 0; i -= 4) {
+			id = parseInt(rawId.substring(i - 4, i), 2).toString(16) + id
+		}
+
+		return BigInt(`0x${id}`)
+	}
+
+	generate(): string {
+		return `${this.generateRaw()}`
+	}
 }
