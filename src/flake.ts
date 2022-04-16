4d7ed0f573082cc7836581d4c8657aa2256a6946
maxichrome <maxichrome@users.noreply.github.com>
2022-04-15 21:08:39 -0500

    wip rust moment

---
+++ src/flake.ts
@@ -0,0 +1,66 @@
+import * as a from "./native";
+
+console.log(a.hello());
+
+export interface FlakeOptions {
+	/**
+	 * Unique machine ID.
+	 */
+	nodeId?: number;
+	/**
+	 * Time, in milliseconds, to use as a custom epoch. Added to UNIX epoch (January 1, 1970).
+	 */
+	timeOffset?: number;
+}
+
+export default class Flake {
+	private sequence: number = 0;
+	private lastTime: number = 0;
+
+	private nodeId: number = 1;
+	private timeOffset: number = 0;
+
+	constructor(options: FlakeOptions) {
+		this.nodeId = (options.nodeId || this.nodeId) % 1023;
+		this.timeOffset = options.timeOffset || this.timeOffset;
+	}
+
+	generateRaw(): BigInt {
+		const nowTime = Date.now();
+		const genTime = (nowTime - this.timeOffset).toString(2);
+
+		this.sequence = 0;
+
+		// set sequence number
+		// this prevents multiple IDs from being generated in 1ms
+		if (this.lastTime === nowTime) {
+			this.sequence += 1;
+
+			if (this.sequence > 4095) {
+				this.sequence = 0;
+
+				// wait until time has incremented by a millisecond
+				while (Date.now() <= nowTime) {}
+			}
+		}
+
+		this.lastTime = nowTime;
+
+		// make sure sequence length will be constant
+		const genSequence = this.sequence.toString(2).padStart(12, "0");
+		const genNode = this.nodeId.toString(2).padStart(10, "0");
+		const rawId = genTime + genNode + genSequence;
+
+		let id = "";
+
+		for (let i = rawId.length; i > 0; i -= 4) {
+			id = parseInt(rawId.substring(i - 4, i), 2).toString(16) + id;
+		}
+
+		return BigInt(`0x${id}`);
+	}
+
+	generate(): string {
+		return `${this.generateRaw()}`;
+	}
+}
---
+++ src/native/lib.rs
@@ -0,0 +1,14 @@
+use neon::prelude::*;
+
+fn generateFlake(nodeId: u8) -> u8 {
+}
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("hello node"))
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("hello", hello)?;
+    Ok(())
+}
